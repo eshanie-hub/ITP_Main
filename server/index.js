be0c4f0b77e8bb01792f680db1e4a9c15389260a
Eshanie <61816545+eshanie-hub@users.noreply.github.com>
2024-04-09 20:23:48 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//import routes
+const inventoryRoutes = require('./routes/inventory.js');
+const cutomerCareRoutes = require('./routes/customer.js');
+const empDetailsRoutes = require('./routes/employee_adding.js');
+const paymentRoutes=require('./routes/payment.js');
+const orderRoutes = require('./routes/order_placement.js');
+const delieveryRoutes = require('./routes/delievery.js');
+const salaryRoutes = require('./routes/employee_salary.js');
+const distributorRouter = require('./routes/distributor.js');
+
+
+//app middlewarennpm 
+app.use(bodyParser.json());
+app.use(cors()); //use to connect backend and frontend
+
+//route middwware
+app.use("/customercare", cutomerCareRoutes);
+app.use("/inventory", inventoryRoutes);
+app.use("/empDetails",empDetailsRoutes);
+app.use("/payment",paymentRoutes);
+app.use("/order_placement", orderRoutes);
+app.use("/delievery", delieveryRoutes);
+app.use("/salary", salaryRoutes);
+app.use("/distributor", distributorRouter);
+
+const PORT  = 8000;
+const DB_URL = 'mongodb+srv://project1:project1@cluster0.b1cggpg.mongodb.net/Vidama'
+
+
+mongoose.connect(DB_URL)
+.then(() => {
+    console.log("DB connected");
+})
+.catch((err) => console.log("DB connection error", err));
+
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`);
+})
