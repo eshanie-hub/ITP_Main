cad098ddc2fc142866503ba7ee0d24a987f98cfb
chathuniabesinghe9 <chathuniabesinghe9@gmail.com>
2024-04-17 21:57:45 +0530

    payment report

--- client/src/pages/payment/Payment_report.js
+++ client/src/pages/payment/Payment_report.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react'
 import Header from '../../component/Header'
-import Chart from "chart.js/auto";
+
 import {
   ResponsiveContainer,
   ComposedChart,
